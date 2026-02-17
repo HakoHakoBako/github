d4e9ad39ae918207235f2725b5592a362b5cc0aa
Nathan Salas <nisalas@ucdavis.edu>
2026-02-17 15:33:07 -0800

    updated js

--- everypictureexperiment/script.js
+++ everypictureexperiment/script.js
@@ -4,30 +4,32 @@ const outlines = document.querySelectorAll(".outline");
 const overlay = document.querySelector("#pictureoverlayorange");
 const closeButton = document.querySelector("#closeButton");
 
-// Hide all outlines
-function resetOutlines() {
-  for (let i = 0; i < outlines.length; i++) {
-    outlines[i].style.opacity = 0;
-  }
-}
-
-// Hover effects
 for (let i = 0; i < hoverZones.length; i++) {
 
   hoverZones[i].addEventListener("mouseenter", function() {
-    resetOutlines();
+
+    // Hide all outlines
+    for (let j = 0; j < outlines.length; j++) {
+      outlines[j].style.opacity = 0;
+    }
+
     mainImage.style.opacity = 0;
     outlines[i].style.opacity = 1;
   });
 
   hoverZones[i].addEventListener("mouseleave", function() {
-    resetOutlines();
+
+    // Hide all outlines
+    for (let j = 0; j < outlines.length; j++) {
+      outlines[j].style.opacity = 0;
+    }
+
     mainImage.style.opacity = 1;
   });
 
 }
 
-// Click overlay (add rest of overlays later)
+// Click overlay (orange)
 hoverZones[0].addEventListener("click", function() {
   overlay.style.display = "flex";
 });
