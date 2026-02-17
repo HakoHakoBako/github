d89c4066adf377ec17d226572f2275e1aad3a0b6
Nathan Salas <nisalas@ucdavis.edu>
2026-02-17 14:06:55 -0800

    refined experiment

--- everypictureexperiment/script.js
+++ everypictureexperiment/script.js
@@ -1,28 +1,39 @@
-let mainImage = document.getElementById("mainImage");
-let hoverZones = document.querySelectorAll(".hover-zone");
-let outlines = document.querySelectorAll(".outline");
+const mainImage = document.getElementById("mainImage");
+const hoverZones = document.querySelectorAll(".hover-zone");
+const outlines = document.querySelectorAll(".outline");
+const overlay = document.getElementById("pictureoverlayorange");
+const closeButton = document.getElementById("closeButton");
 
-for (let i = 0; i < hoverZones.length; i++) {
-
-  hoverZones[i].addEventListener("mouseenter", function() {
-
-    resetOutlines(); 
-    mainImage.style.opacity = 0;
-    outlines[i].style.opacity = 1;
-
-  });
-
-  hoverZones[i].addEventListener("mouseleave", function() {
-
-    resetOutlines();
-    mainImage.style.opacity = 1;
-
-  });
-
-}
-
+// Hide all outlines
 function resetOutlines() {
   for (let i = 0; i < outlines.length; i++) {
     outlines[i].style.opacity = 0;
   }
 }
+
+// Loop through hover zones
+for (let i = 0; i < hoverZones.length; i++) {
+
+  hoverZones[i].addEventListener("mouseenter", function() {
+    resetOutlines();
+    mainImage.style.opacity = 0;
+    outlines[i].style.opacity = 1;
+  });
+
+  hoverZones[i].addEventListener("mouseleave", function() {
+    resetOutlines();
+    mainImage.style.opacity = 1;
+  });
+
+}
+
+// Click overlay (add the other hover zones later)
+hoverZones[0].addEventListener("click", function() {
+  overlay.style.display = "flex";
+});
+
+// Close overlay
+closeButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  overlay.style.display = "none";
+});
