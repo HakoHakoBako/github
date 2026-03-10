cbd4ee05644b8655c70ab8a87132a29d83e06915
Nathan Salas <nisalas@ucdavis.edu>
2026-03-10 11:04:14 -0700

    added some changes

--- gameonv1htmlcss/script.js
+++ gameonv1htmlcss/script.js
@@ -1,242 +1,255 @@
 (function(){
 
-"use strict";
+    "use strict";
 
-console.log("reading.js");
+    console.log("reading.js");
 
-/* ---------------- OVERLAY ELEMENTS ---------------- */
+    // Overlay
 
-const overlay = document.querySelector("#avatarOverlay");
-const confirmBtn = document.querySelector("#confirmSelection");
+    const overlay = document.querySelector("#avatarOverlay");
+    const confirmBtn = document.querySelector("#confirmSelection");
 
-const p1AvatarPreview = document.querySelector("#p1Avatar");
-const p2AvatarPreview = document.querySelector("#p2Avatar");
+    const p1AvatarPreview = document.querySelector("#p1Avatar");
+    const p2AvatarPreview = document.querySelector("#p2Avatar");
 
-const p1Left = document.querySelector("#p1Left");
-const p1Right = document.querySelector("#p1Right");
+    const p1Left = document.querySelector("#p1Left");
+    const p1Right = document.querySelector("#p1Right");
 
-const p2Left = document.querySelector("#p2Left");
-const p2Right = document.querySelector("#p2Right");
+    const p2Left = document.querySelector("#p2Left");
+    const p2Right = document.querySelector("#p2Right");
 
-/* ---------------- GAME INTERFACE ---------------- */
+    const p1Name = document.querySelector("#p1Name");
+    const p2Name = document.querySelector("#p2Name");
 
-const gameArea = document.querySelector("#game");
 
-const rollBtn = document.querySelector("#roll");
-const passBtn = document.querySelector("#pass");
+    // Game UI
 
-const scoreP1 = document.querySelector("#scoreP1");
-const scoreP2 = document.querySelector("#scoreP2");
+    const gameArea = document.querySelector("#game");
 
-const avatarP1 = document.querySelector("#player1Avatar");
-const avatarP2 = document.querySelector("#player2Avatar");
+    const rollBtn = document.querySelector("#roll");
+    const passBtn = document.querySelector("#pass");
+    const newGameBtn = document.querySelector("#newGame");
 
-const titleMessage = document.querySelector(".title");
+    const scoreP1 = document.querySelector("#scoreP1");
+    const scoreP2 = document.querySelector("#scoreP2");
 
-/* ---------------- AVATAR OPTIONS ---------------- */
+    const avatarP1 = document.querySelector("#player1Avatar");
+    const avatarP2 = document.querySelector("#player2Avatar");
 
-const penguins = [
-"images/penguinChef.svg",
-"images/penguinBarista.svg",
-"images/penguinMagician.svg",
-"images/penguinSailor.svg"
-];
+    const titleMessage = document.querySelector(".title");
 
-let p1Index = 0;
-let p2Index = 1;
 
-/* ---------------- UPDATE AVATAR PREVIEW ---------------- */
+    // Penguin Avatars
 
-function updateAvatar(player){
+    const penguins = ["images/penguinChef.svg","images/penguinBarista.svg","images/penguinMagician.svg","images/penguinSailor.svg"];
 
-if(player === 1){
-p1AvatarPreview.src = penguins[p1Index];
-}
+    const penguinNames = ["Chef", "Barista", "Magician", "Sailor"];
 
-if(player === 2){
-p2AvatarPreview.src = penguins[p2Index];
-}
+    let p1Index = 0;
+    let p2Index = 1;
 
-}
+    // Changing the Avatar
 
-/* ---------------- AVATAR ARROWS ---------------- */
+    function updateAvatar(player){
 
-p1Left.addEventListener("click", function(){
+        if(player === 1){
+            p1AvatarPreview.src = penguins[p1Index];
+            p1Name.textContent = penguinNames[p1Index];
+        }
 
-p1Index = (p1Index - 1 + penguins.length) % penguins.length;
-updateAvatar(1);
+        if(player === 2){
+            p2AvatarPreview.src = penguins[p2Index];
+            p2Name.textContent = penguinNames[p2Index];
+        }
 
-});
+    }
 
-p1Right.addEventListener("click", function(){
 
-p1Index = (p1Index + 1) % penguins.length;
-updateAvatar(1);
+    // Arrows changing the avatar
 
-});
+    p1Left.addEventListener("click", function(){
 
-p2Left.addEventListener("click", function(){
+        p1Index = (p1Index - 1 + penguins.length) % penguins.length;
+        updateAvatar(1);
 
-p2Index = (p2Index - 1 + penguins.length) % penguins.length;
-updateAvatar(2);
+    });
 
-});
+    p1Right.addEventListener("click", function(){
 
-p2Right.addEventListener("click", function(){
+        p1Index = (p1Index + 1) % penguins.length;
+        updateAvatar(1);
 
-p2Index = (p2Index + 1) % penguins.length;
-updateAvatar(2);
+    });
 
-});
+    p2Left.addEventListener("click", function(){
 
-/* ---------------- GAME DATA ---------------- */
+        p2Index = (p2Index - 1 + penguins.length) % penguins.length;
+        updateAvatar(2);
 
-const gameData = {
+    });
 
-dice:[
-"dice1.svg",
-"dice2.svg",
-"dice3.svg",
-"dice4.svg",
-"dice5.svg",
-"dice6.svg"
-],
+    p2Right.addEventListener("click", function(){
 
-players:["Player 1","Player 2"],
+        p2Index = (p2Index + 1) % penguins.length;
+        updateAvatar(2);
 
-score:[0,0],
+    });
 
-roll1:0,
-roll2:0,
-rollSum:0,
 
-index:0,
+    // Game data with the dice images
 
-gameEnd:25
+    const gameData = {
 
-};
+        dice:["dice1.svg","dice2.svg","dice3.svg","dice4.svg","dice5.svg","dice6.svg"],
 
-/* ---------------- START GAME AFTER CONFIRM ---------------- */
+        players:["Player 1","Player 2"],
 
-confirmBtn.addEventListener("click", function(){
+        score:[0,0],
 
-avatarP1.innerHTML = `<img src="${penguins[p1Index]}" width="150">`;
-avatarP2.innerHTML = `<img src="${penguins[p2Index]}" width="150">`;
+        roll1:0,
+        roll2:0,
+        rollSum:0,
+        index:0,
+        gameEnd:25
+    };
 
-overlay.style.display = "none";
+    // Game Start after overlay
 
-gameData.index = Math.round(Math.random());
+    confirmBtn.addEventListener("click", function(){
 
-titleMessage.textContent = `${gameData.players[gameData.index]}'s Turn - Roll the Ice!`;
+        avatarP1.innerHTML = `<img src="${penguins[p1Index]}" width="150">`;
+        avatarP2.innerHTML = `<img src="${penguins[p2Index]}" width="150">`;
 
-updateScores();
+        overlay.style.display = "none";
 
-});
+        gameData.index = Math.round(Math.random());
 
-/* ---------------- ROLL BUTTON ---------------- */
+        titleMessage.textContent =
+            `${gameData.players[gameData.index]}'s Turn - Roll the Ice!`;
 
-rollBtn.addEventListener("click", function(){
+        updateScores();
 
-throwDice();
+    });
 
-});
 
-/* ---------------- PASS BUTTON ---------------- */
+    // Roll Button
 
-passBtn.addEventListener("click", function(){
+    rollBtn.addEventListener("click", function(){
 
-switchPlayer();
+        throwDice();
 
-});
+    });
 
-/* ---------------- THROW DICE ---------------- */
 
-function throwDice(){
+    // Pass Button
 
-gameData.roll1 = Math.floor(Math.random()*6)+1;
-gameData.roll2 = Math.floor(Math.random()*6)+1;
+    passBtn.addEventListener("click", function(){
 
-gameData.rollSum = gameData.roll1 + gameData.roll2;
+        switchPlayer();
 
-gameArea.innerHTML = `
-<img src="images/${gameData.dice[gameData.roll1-1]}" width="80">
-<img src="images/${gameData.dice[gameData.roll2-1]}" width="80">
-`;
+    });
 
-/* snake eyes */
 
-if(gameData.rollSum === 2){
+    // Throwing the Dice
 
-titleMessage.textContent = "Oh no you broke the ice! Score reset!";
+    function throwDice(){
 
-gameData.score[gameData.index] = 0;
+        gameData.roll1 = Math.floor(Math.random()*6) + 1;
+        gameData.roll2 = Math.floor(Math.random()*6) + 1;
 
-updateScores();
+        gameData.rollSum = gameData.roll1 + gameData.roll2;
 
-setTimeout(function(){
-switchPlayer();
-},1500);
+        gameArea.innerHTML = `<img src="images/${gameData.dice[gameData.roll1-1]}" width="80"> <img src="images/${gameData.dice[gameData.roll2-1]}" width="80">`;
 
-}
+    // Snake Eyes (Breaking Ice)
 
-/* rolled a 1 */
+        if(gameData.rollSum === 2){
 
-else if(gameData.roll1 === 1 || gameData.roll2 === 1){
+            titleMessage.textContent =
+                "Oh no you broke the ice! Score reset!";
 
-titleMessage.textContent = "Rolled a 1! Next Turn!";
+            gameData.score[gameData.index] = 0;
 
-setTimeout(function(){
-switchPlayer();
-},1500);
+            updateScores();
 
-}
+            setTimeout(function(){
+                switchPlayer();
+            },1500);
+        }
 
-/* normal roll */
+        // If player rolls a 1
 
-else{
+        else if(gameData.roll1 === 1 || gameData.roll2 === 1){
 
-gameData.score[gameData.index] += gameData.rollSum;
+            titleMessage.textContent = "Rolled a 1! Next Turn!";
 
-updateScores();
+            setTimeout(function(){
+                switchPlayer();
+            },1500);
+        }
 
-checkWinner();
+        // Normal Dice Roll
 
-}
+        else{
 
-}
+            gameData.score[gameData.index] += gameData.rollSum;
 
-/* ---------------- SWITCH PLAYER ---------------- */
+            updateScores();
 
-function switchPlayer(){
+            checkWinner();
+        }
 
-gameData.index = gameData.index === 0 ? 1 : 0;
+    }
 
-titleMessage.textContent = `${gameData.players[gameData.index]}'s Turn - Roll the Ice!`;
 
-}
+    // Switching Players
 
-/* ---------------- UPDATE SCORES ---------------- */
+    function switchPlayer(){
 
-function updateScores(){
+        gameData.index = gameData.index === 0 ? 1 : 0;
 
-scoreP1.textContent = `Score: ${gameData.score[0]}`;
-scoreP2.textContent = `Score: ${gameData.score[1]}`;
+        titleMessage.textContent =
+            `${gameData.players[gameData.index]}'s Turn - Roll the Ice!`;
 
-}
+    }
 
-/* ---------------- WIN CONDITION ---------------- */
 
-function checkWinner(){
+    // Updating the score
 
-if(gameData.score[gameData.index] >= gameData.gameEnd){
+    function updateScores(){
 
-titleMessage.textContent = `${gameData.players[gameData.index]} Wins!`;
+        scoreP1.textContent = `Score: ${gameData.score[0]}`;
+        scoreP2.textContent = `Score: ${gameData.score[1]}`;
 
-rollBtn.disabled = true;
-passBtn.disabled = true;
+    }
 
-}
 
-}
+    // Winning Condition
+
+    function checkWinner(){
+
+        if(gameData.score[gameData.index] > gameData.gameEnd || 
+   gameData.score[gameData.index] === gameData.gameEnd){
+
+            titleMessage.textContent =
+                `${gameData.players[gameData.index]} Wins!`;
+
+            rollBtn.disabled = true;
+            passBtn.disabled = true;
+
+            newGameBtn.style.display = "inline-block";
+
+        }
+
+    }
+
+
+    // Start a new game
+
+    newGameBtn.addEventListener("click", function(){
+
+        location.reload();
+
+    });
 
 })();
